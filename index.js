84deaf7f6d2f9e6af1a3e22882a999b502aa8001
Sharoze Archer <sharoze.archer@gmail.com>
2023-03-17 18:53:35 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(height) {
+
+  if(height <= 400) {
+    return "This one is on me!";
+  }
+
+  else if (height <= 2000 ) {
+    return 'That will be twenty bucks.';
+  }
+
+  else if (height <= 2500 ) {
+    return 'I will gladly take your thirty bucks.';
+  }
+
+  else {
+    return 'No can do.';
+  }
+
+
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(199));
+
+console.log(scuberGreetingForFeet(1500));
+
+console.log(scuberGreetingForFeet(2001));
+
+console.log(scuberGreetingForFeet(2501));
+
+
+function ternaryCheckCity(city){
+
+  return city === "NYC" ?   "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Pittsburgh'));
+
+function switchOnCharmFromTip(tip){
+
+  switch(tip) {
+
+    case "generous":
+      return "Thank you so much.";
+
+    case "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye.";
+  }
 }
+
+console.log(switchOnCharmFromTip('generous'));
+
+console.log(switchOnCharmFromTip('not as generous'));
+
+console.log(switchOnCharmFromTip('thanks for everything'));
+  
